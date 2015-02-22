33738fb1d09ecfb8b8ee765f4e4465642345982b
Christophe Rosset <tophe@topheman.com>
2015-02-22 19:18:01 +0100

    cors pattern unit testing

--- test/spec/handlers.config.cors.spec.js
+++ test/spec/handlers.config.cors.spec.js
@@ -58,4 +58,117 @@ describe('Handlers configuration - CORS part', function () {
     });
   });
 
+  describe('Check CORS pattern domains', function () {
+    
+    describe('Access restricted to cors = "foo.com"', function () {
+      it('request from foo.com should accept CORS', function (done) {
+        request(app)
+                .get('/testCorsPattern1')
+                .set('Origin', 'http://foo.com')
+                .expect(200)
+                .expect('Access-Control-Allow-Origin', 'http://foo.com')
+                .end(done);
+      });
+      it('request NOT from foo.com should deny CORS', function (done) {
+        request(app)
+                .get('/testCorsPattern1')
+                .expect(200)
+                .set('Origin', 'http://anywhere.com')
+                .expect(function (res) {
+                  if (res.headers['Access-Control-Allow-Origin'.toLowerCase()]) {
+                    return "Access-Control-Allow-Origin header is present and shouldn't";
+                  }
+                })
+                .end(done);
+      });
+    });
+    
+    describe('Access restricted to cors = ["foo.com","bar.com"]',function(){
+      it('request from foo.com should accept CORS', function (done) {
+        request(app)
+                .get('/testCorsPattern2')
+                .set('Origin', 'http://foo.com')
+                .expect(200)
+                .expect('Access-Control-Allow-Origin', 'http://foo.com')
+                .end(done);
+      });
+      it('request NOT from foo.com should deny CORS', function (done) {
+        request(app)
+                .get('/testCorsPattern2')
+                .expect(200)
+                .set('Origin', 'http://anywhere.com')
+                .expect(function (res) {
+                  if (res.headers['Access-Control-Allow-Origin'.toLowerCase()]) {
+                    return "Access-Control-Allow-Origin header is present and shouldn't";
+                  }
+                })
+                .end(done);
+      });
+      it('request from bar.com should accept CORS', function (done) {
+        request(app)
+                .get('/testCorsPattern2')
+                .set('Origin', 'http://bar.com')
+                .expect(200)
+                .expect('Access-Control-Allow-Origin', 'http://bar.com')
+                .end(done);
+      });
+      it('request NOT from bar.com should deny CORS', function (done) {
+        request(app)
+                .get('/testCorsPattern2')
+                .expect(200)
+                .set('Origin', 'http://anywhere.com')
+                .expect(function (res) {
+                  if (res.headers['Access-Control-Allow-Origin'.toLowerCase()]) {
+                    return "Access-Control-Allow-Origin header is present and shouldn't";
+                  }
+                })
+                .end(done);
+      });
+    });
+    
+    describe('Access restricted to cors = ["*.foo.com","bar.com"]',function(){
+      it('request from foo.com should accept CORS', function (done) {
+        request(app)
+                .get('/testCorsPattern3')
+                .set('Origin', 'http://foo.com')
+                .expect(200)
+                .expect('Access-Control-Allow-Origin', 'http://foo.com')
+                .end(done);
+      });
+      it('request NOT from foo.com should deny CORS', function (done) {
+        request(app)
+                .get('/testCorsPattern3')
+                .expect(200)
+                .set('Origin', 'http://anywhere.com')
+                .expect(function (res) {
+                  if (res.headers['Access-Control-Allow-Origin'.toLowerCase()]) {
+                    return "Access-Control-Allow-Origin header is present and shouldn't";
+                  }
+                })
+                .end(done);
+      });
+      it('request from subdomain.foo.com should accept CORS', function (done) {
+        request(app)
+                .get('/testCorsPattern3')
+                .set('Origin', 'http://subdomain.foo.com')
+                .expect(200)
+                .expect('Access-Control-Allow-Origin', 'http://subdomain.foo.com')
+                .end(done);
+      });
+      it('request NOT from subdomain.bar.com should deny CORS', function (done) {
+        request(app)
+                .get('/testCorsPattern3')
+                .expect(200)
+                .set('Origin', 'http://subdomain.bar.com')
+                .expect(function (res) {
+                  if (res.headers['Access-Control-Allow-Origin'.toLowerCase()]) {
+                    return "Access-Control-Allow-Origin header is present and shouldn't";
+                  }
+                })
+                .end(done);
+      });
+    });
+    
+  });
+
 });
