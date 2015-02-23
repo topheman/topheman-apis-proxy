0f46ec1aaafdc63da5ec47f2a9cea6841ffc26ea
Christophe Rosset <tophe@topheman.com>
2015-02-23 22:59:25 +0100

    first draft of github router

--- apis/github/router.js
+++ apis/github/router.js
@@ -7,12 +7,16 @@
 
 var express = require('express');
 var router = express.Router();
+var proxy = require('express-http-proxy');
+var CURRENT_HANDLER_BASE_PATH = require('./index').endpoint;
+var CURRENT_HANDLER_BASE_PATH_REGEX_REPLACE = new RegExp('^'+CURRENT_HANDLER_BASE_PATH);
+var GITHUB_API_BASE_PATH = 'https://api.github.com';
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-  res.json({
-    "list of apis" : []
-  });
-});
+router.get('/*', proxy(GITHUB_API_BASE_PATH,{
+  forwardPath: function(req, res) {
+    return req.originalUrl.replace(CURRENT_HANDLER_BASE_PATH_REGEX_REPLACE,"");
+  }
+}));
 
 module.exports = router;
