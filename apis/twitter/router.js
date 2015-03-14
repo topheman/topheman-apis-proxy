814b9cb1f2f8bdfe2259ac16f5ad44d12232d92c
Christophe Rosset <tophe@topheman.com>
2015-03-14 18:57:48 +0100

    twitter : prevent browser to cache responses (removed Etag header)

--- apis/twitter/router.js
+++ apis/twitter/router.js
@@ -16,33 +16,40 @@ var T = new Twit({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-var routes = {
-  user_show_by_user_id: '/users/show/:user_id(\\d+)',
-  user_show_by_screen_name: '/users/show/:screen_name(\\w+)',
-  statuses_show_by_id: '/statuses/show/:id(\\d+)'
-};
-
 router.get('/', function (req, res) {
-  res.set('Content-Type','text/html');
-  res.render(__dirname+'/root.ejs', {
+  res.set('Content-Type', 'text/html');
+  res.render(__dirname + '/root.ejs', {
     layout: 'layout',
-    title : apiDescription.title,
-    description : apiDescription.description
+    title: apiDescription.title,
+    description: apiDescription.description
   });
 });
 
 router.get('/*', function (req, res, next) {
   //remove leading slash (Twit won't take it) + remove extension to be compliant with the default rest api (though we response only with json)
-  var url = req.path.substr(1).replace('.json','').replace('.xml','');
+  var url = req.path.substr(1).replace('.json', '').replace('.xml', '');
   T.get(url, req.query, function (err, data, response) {
     if (err) {
       next(err);
     }
     else {
+      //prevent browser to cache request
+
+      //twitter returns data without cache control aka : always HTTP 200
+      //from our side, if we leave cache control, the request to twitter will be done,
+      //but since our express server delivers cache control headers to the client,
+      //it will deliver 304 then the browser won't care about the response
+
+      //HACK to workaround the framework sending e-tags and "304 NOT MODIFIED" responses
+      //sorry, no right way to do it, more here : http://stackoverflow.com/a/19947147/2733488
+      req.method = "NONE"; // was "GET"
+
       //transfer headers containing limit infos
       res.set('x-rate-limit-limit', response.headers['x-rate-limit-limit']);
       res.set('x-rate-limit-remaining', response.headers['x-rate-limit-remaining']);
       res.set('x-rate-limit-reset', response.headers['x-rate-limit-reset']);
+
+      //finally send fresh data
       res.json(data);
     }
   });
